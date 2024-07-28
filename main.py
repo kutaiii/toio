4c55751be2639192eff2a8c8de8187d8a6c24726
onigirihk <64179627+onigirihk@users.noreply.github.com>
2024-07-29 07:46:07 +0900

    add comment

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 import signal
 import matplotlib.pyplot as plt
 
+#TODO　toioの移動とmappingを同時に行うため、全体を非同期処理に変更する　Change the entire process to asynchronous processing in order to move and map toio at the same time
+
 LOOP = True
 ATOM_MAC = "64:b7:08:80:e1:3c" #bluetooth接続に使用　環境にあわせて変更  change to suit your environment
 ATOM_IP = "192.168.0.104" #WiFi接続に使用　環境にあわせて変更  change to suit your environment
@@ -14,7 +16,7 @@ MAX_X = 78
 MIN_X = -205
 MAX_Y = 205
 MIN_Y = -13
-config = MappingConfig(MAX_X, MIN_X, MAX_Y, MIN_Y)
+MAPCONFIG = MapSetting(MAX_X, MIN_X, MAX_Y, MIN_Y)
 
 def ctrl_c_handler(_signum, _frame):
     global LOOP
@@ -23,10 +25,10 @@ def ctrl_c_handler(_signum, _frame):
 signal.signal(signal.SIGINT, ctrl_c_handler)
 
 
-def main(config):
+def main():
     cube = SimpleCube()
     atom = AtomSerialConnection(PORT) #WiFi接続の場合はAtomWiFiConnection(ATOM_IP)に変更
-    slam = SLAM(atom, cube, config)
+    slam = SLAM(atom, cube, MAPCONFIG)
     fig,ax = plt.subplots()
     fig2,ax2 = plt.subplots()
     im = ax.imshow(slam.get_map(), cmap='jet', vmin=0, vmax=2)
@@ -46,4 +48,4 @@ def main(config):
 
 
 if __name__ == "__main__":
-    sys.exit(main(config))
+    sys.exit(main())
